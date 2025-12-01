d60aff61f4f6cc2f8aca6fbd41c3f6f2c8a89457
bluediamondinteriorschingoli-max <bluediamondinteriorschingoli@gmail.com>
2025-12-01 12:54:31 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,327 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-tv-unit",
+      "name": "TV UNIT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.008158269635373472,
+        "pitch": -0.0024343152498929044,
+        "fov": 0.9067824305845541
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8601349821543076,
+          "pitch": 0.02973823570881251,
+          "rotation": 0,
+          "target": "5-wash-unit"
+        },
+        {
+          "yaw": 0.5865047984257146,
+          "pitch": -0.036856480697329275,
+          "rotation": 4.71238898038469,
+          "target": "2-book-shelf"
+        },
+        {
+          "yaw": 0.7311366244064654,
+          "pitch": 0.005996441554977139,
+          "rotation": 0.7853981633974483,
+          "target": "1-breakfast"
+        },
+        {
+          "yaw": 1.1684897423849385,
+          "pitch": 0.00047216516752257576,
+          "rotation": 0,
+          "target": "4-kitchen-worker-area"
+        },
+        {
+          "yaw": 1.0662582606495228,
+          "pitch": -0.050737025164353966,
+          "rotation": 5.497787143782138,
+          "target": "3-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-breakfast",
+      "name": "breakfast",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.0026072207212592957,
+        "pitch": 0.020048687601429194,
+        "fov": 1.3146575614876592
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9616890285292854,
+          "pitch": 0.13061524886602172,
+          "rotation": 4.71238898038469,
+          "target": "2-book-shelf"
+        },
+        {
+          "yaw": -0.21298500172582813,
+          "pitch": -0.0070113460639706915,
+          "rotation": 10.995574287564278,
+          "target": "3-kitchen"
+        },
+        {
+          "yaw": 0.0029499564377974963,
+          "pitch": 0.04635078601955378,
+          "rotation": 0,
+          "target": "4-kitchen-worker-area"
+        },
+        {
+          "yaw": 0.8424392560430665,
+          "pitch": 0.0684591642761525,
+          "rotation": 5.497787143782138,
+          "target": "5-wash-unit"
+        },
+        {
+          "yaw": 2.0559432040418653,
+          "pitch": 0.23900306320494735,
+          "rotation": 2.356194490192345,
+          "target": "0-tv-unit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-book-shelf",
+      "name": "BOOK SHELF",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.21171849836620105,
+        "pitch": -0.02829989038127856,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5362726792115318,
+          "pitch": 0.13125314211510997,
+          "rotation": 0.7853981633974483,
+          "target": "1-breakfast"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kitchen",
+      "name": "KITCHEN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.05255588271248257,
+        "pitch": -0.0064238524092203875,
+        "fov": 0.9518719259543815
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8122190687222233,
+          "pitch": 0.09728511179894284,
+          "rotation": 0.7853981633974483,
+          "target": "4-kitchen-worker-area"
+        },
+        {
+          "yaw": -0.9336003170240019,
+          "pitch": -0.06377965393284768,
+          "rotation": 4.71238898038469,
+          "target": "2-book-shelf"
+        },
+        {
+          "yaw": -1.3468124379588247,
+          "pitch": 0.08122436992272064,
+          "rotation": 1.5707963267948966,
+          "target": "1-breakfast"
+        },
+        {
+          "yaw": -2.0215983605407644,
+          "pitch": 0.0644209865860148,
+          "rotation": 1.5707963267948966,
+          "target": "0-tv-unit"
+        },
+        {
+          "yaw": 2.6780906963487983,
+          "pitch": 0.06092857834764054,
+          "rotation": 10.995574287564278,
+          "target": "5-wash-unit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kitchen-worker-area",
+      "name": "KITCHEN WORKER AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.15305194979027625,
+        "pitch": 0.040815880025853346,
+        "fov": 1.2579409239652903
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.071264978888058,
+          "pitch": 0.139985526564395,
+          "rotation": 0,
+          "target": "3-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-wash-unit",
+      "name": "WASH UNIT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.006578351086965029,
+        "pitch": 0.02157706636474188,
+        "fov": 1.0046821526083987
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.606159483078125,
+          "pitch": 0.10593245901672965,
+          "rotation": 0.7853981633974483,
+          "target": "0-tv-unit"
+        },
+        {
+          "yaw": -1.541499399585879,
+          "pitch": 0.06126701644936183,
+          "rotation": 0,
+          "target": "3-kitchen"
+        },
+        {
+          "yaw": -1.3110398006730293,
+          "pitch": 0.02270327456498933,
+          "rotation": 1.5707963267948966,
+          "target": "4-kitchen-worker-area"
+        },
+        {
+          "yaw": -2.119370256435996,
+          "pitch": -0.015923418055194816,
+          "rotation": 11.780972450961727,
+          "target": "2-book-shelf"
+        },
+        {
+          "yaw": -1.9885567196350564,
+          "pitch": 0.05317843581984505,
+          "rotation": 0,
+          "target": "1-breakfast"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ANEESH",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
